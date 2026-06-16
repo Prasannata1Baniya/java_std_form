0cff185ffd0454e85348e4550e121f1b97c084bf
Prasannata Baniya <prasannata250baniya@gmail.com>
2026-06-16 15:48:00 +0545

    Initial commit with servlets and gitignore

---
+++ src/main/java/com/student/com/DBConnection.java
@@ -0,0 +1,5 @@
+package com.student.com;
+
+public class DBConnection {
+
+}
---
+++ src/main/java/com/student/com/StudentServlet.java
@@ -0,0 +1,60 @@
+package com.student.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public StudentServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out =response.getWriter();
+		
+		
+		String name= request.getParameter("name");
+		String city=request.getParameter("city");
+		
+	   try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root" ,"");
+			String query = "INSERT INTO companies(name,city) VALUES(?,?)";
+			PreparedStatement ps= con.prepareStatement(query);
+			
+			ps.setString(1, name);
+			ps.setString(2, city);
+			
+			int result = ps.executeUpdate();
+			
+			if(result>0) {
+				out.println("Data Saved Successfully");
+			}
+			else {
+				out.println("No data saved");
+			}
+			
+			con.close();
+				   
+	   }catch(Exception e) {
+		out.println("Error:"+e.getMessage()); 
+		e.printStackTrace();
+	   }
+		
+	}
+
+}
